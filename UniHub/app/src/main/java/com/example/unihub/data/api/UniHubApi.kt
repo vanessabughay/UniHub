445bfd43f8f49a3ede7b81775bdfdda207ffc82e
Victoria Isabelle Corbolin Monte <vblmqs@gmail.com>
2025-08-27 23:54:07 -0300

    conexão com backend

---
+++ UniHub/app/src/main/java/com/example/unihub/data/api/UniHubApi.kt
@@ -0,0 +1,15 @@
+package com.example.unihub.data.api
+
+import com.example.unihub.data.api.model.LoginRequest
+import com.example.unihub.data.api.model.RegisterRequest
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+interface UniHubApi {
+    @POST("api/auth/register")
+    suspend fun registerUser(@Body request: RegisterRequest): Response<Void>
+
+    @POST("api/auth/login")
+    suspend fun loginUser(@Body request: LoginRequest): Response<Void>
+}
