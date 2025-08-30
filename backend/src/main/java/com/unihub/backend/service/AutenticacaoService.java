3037220bbc15b68ec0ebe7fcf2dc0b48f87771fe
vanessabughay <vanessagaebler@gmail.com>
2025-08-30 18:27:38 -0300

    login (ainda com erro)

--- backend/src/main/java/com/unihub/backend/service/AutenticacaoService.java
+++ backend/src/main/java/com/unihub/backend/service/AutenticacaoService.java
@@ -1,5 +1,6 @@
 package com.unihub.backend.service;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import com.unihub.backend.model.Usuario;
 import com.unihub.backend.repository.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +15,21 @@ public class AutenticacaoService {
     @Autowired
     private UsuarioRepository repository;
 
+     @Autowired
+    private PasswordEncoder passwordEncoder;
+
     public Usuario registrar(Usuario usuario) {
         Optional<Usuario> existente = repository.findByEmail(usuario.getEmail());
         if (existente.isPresent()) {
             throw new IllegalArgumentException("E-mail já cadastrado");
         }
+         usuario.setSenha(passwordEncoder.encode(usuario.getSenha()));
         return repository.save(usuario);
     }
 
     public String login(String email, String senha) {
         Optional<Usuario> usuarioOpt = repository.findByEmail(email);
-        if (usuarioOpt.isPresent() && usuarioOpt.get().getSenha().equals(senha)) {
+         if (usuarioOpt.isPresent() && passwordEncoder.matches(senha, usuarioOpt.get().getSenha())) {
             return UUID.randomUUID().toString();
         }
         return null;
