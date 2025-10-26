9fa1a935cba82d46e1a2c1fa70826f1f17d881a2
Rafaella Nascimento <lafaella@hotmail.com>
2025-10-26 18:36:39 -0300

    Adicionando usuario.kt

---
+++ UniHub/app/src/main/java/com/example/unihub/data/model/Usuario.kt
@@ -0,0 +1,32 @@
+package com.example.unihub.data.model
+
+data class UsuarioResumo(
+    val id: Long,
+    val nome: String,
+    val email: String
+)
+
+data class CompartilharDisciplinaRequest(
+    val disciplinaId: Long,
+    val remetenteId: Long,
+    val destinatarioId: Long,
+    val mensagem: String? = null
+)
+
+data class ConviteCompartilhamentoResponse(
+    val id: Long,
+    val disciplinaId: Long?,
+    val remetenteId: Long?,
+    val destinatarioId: Long?,
+    val status: String,
+    val mensagem: String?
+)
+
+data class NotificacaoResponse(
+    val id: Long,
+    val mensagem: String,
+    val lida: Boolean,
+    val tipo: String?,
+    val conviteId: Long?,
+    val criadaEm: String?
+)
