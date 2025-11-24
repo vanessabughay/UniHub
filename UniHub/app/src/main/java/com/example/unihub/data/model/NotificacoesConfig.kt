de921a196abc5dc1d17c26d06453d2403f43f018
Rafaella Nascimento <lafaella@hotmail.com>
2025-11-24 19:34:29 -0300

    Máscaras notificações config

--- UniHub/app/src/main/java/com/example/unihub/data/model/NotificacoesConfig.kt
+++ UniHub/app/src/main/java/com/example/unihub/data/model/NotificacoesConfig.kt
@@ -1,6 +1,9 @@
 package com.example.unihub.data.model
 
+import com.google.gson.annotations.SerializedName
+
 data class AvaliacoesConfig(
+    @SerializedName(value = "antecedencia", alternate = ["antecedencia_map"])
     val antecedencia: Map<Prioridade, Antecedencia> = defaultAntecedencias()
 )
 
@@ -9,18 +12,27 @@ fun defaultAntecedencias(): Map<Prioridade, Antecedencia> =
 
 data class NotificacoesConfig(
     // Seção Disciplinas
+    @SerializedName(value = "notificacaoDePresenca", alternate = ["notificacao_de_presenca"])
     val notificacaoDePresenca: Boolean = true,
+    @SerializedName(value = "avaliacoesAtivas", alternate = ["avaliacoes_ativas"])
     val avaliacoesAtivas: Boolean = true,
+    @SerializedName(value = "avaliacoesConfig", alternate = ["avaliacoes_config"])
     val avaliacoesConfig: AvaliacoesConfig = AvaliacoesConfig(),
+    @SerializedName(value = "compartilhamentoDisciplina", alternate = ["compartilhamento_disciplina"])
     val compartilhamentoDisciplina: Boolean = true,
 
     // Seção Quadros/Tarefas
+    @SerializedName(value = "incluirEmQuadro", alternate = ["incluir_em_quadro"])
     val incluirEmQuadro: Boolean = true,
+    @SerializedName(value = "prazoTarefa", alternate = ["prazo_tarefa"])
     val prazoTarefa: Boolean = true,
+    @SerializedName(value = "comentarioTarefa", alternate = ["comentario_tarefa"])
     val comentarioTarefa: Boolean = true,
 
     // Seção Contatos/Grupos
+    @SerializedName(value = "conviteContato", alternate = ["convite_contato"])
     val conviteContato: Boolean = true,
+    @SerializedName(value = "inclusoEmGrupo", alternate = ["incluso_em_grupo"])
     val inclusoEmGrupo: Boolean = true
 )
 
